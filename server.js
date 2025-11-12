57defc0bb1c16f882afe4575e2839c675f84977d
rodrigocsar <rodrigocesar068@gmail.com>
2025-11-12 08:38:20 +0044

    ajuste server

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
+// Rota principal do webhook
 app.post("/", (req, res) => {
   console.log("📩 Dados recebidos:", req.body);
-  res.status(200).json({ success: true });
+
+  // Garante que retorna um objeto com as chaves esperadas pela Pluga
+  const dados = {
+    nome: req.body.nome,
+    email: req.body.email,
+    data_teste: req.body.data_teste
+  };
+
+  res.status(200).json(dados);
 });
 
+// Apenas rota de teste para ver se o servidor está no ar
 app.get("/", (req, res) => {
   res.send("Servidor da Atividade Pluga rodando!");
 });
